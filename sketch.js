f0e1245ab975f742cfdc4f501c07ffb746d52ec3
Wilson Santos <contato@wilsonsantos.net>
2020-06-24 15:40:43 -0300

    Add new Enimies

--- sketch.js
+++ sketch.js
@@ -60,12 +60,67 @@ const matrizInimigo = [
     [104, 626],
     [208, 626],
     [312, 626],
-]
+];
+
+const matrizInimigoGrande = [
+  [0,0],
+  [400,0],
+  [800,0],
+  [1200,0],
+  [1600,0],
+  [0,400],
+  [400,400],
+  [800,400],
+  [1200, 400],
+  [1600, 400],
+  [0,800],
+  [400, 800],
+  [800, 800],
+  [1200, 800],
+  [1600, 800],
+  [0, 1200],
+  [400, 1200],
+  [800, 1200],
+  [1200, 1200],
+  [1600, 1200], 
+  [0, 1600],
+  [400, 1600],
+  [800, 1600],
+  [1200, 1600],
+  [1600, 1600],
+  [0, 2000],
+  [400, 2000],
+  [800, 2000],
+];
+
+const matrizInimigoVoador = [
+  [0,0],
+  [200, 0],
+  [400, 0],
+  [0, 150],
+  [200, 150],
+  [400, 150],
+  [0, 300],
+  [200, 300],
+  [400, 300],
+  [0, 450],
+  [200, 450],
+  [400, 450],
+  [0, 600],
+  [200, 600],
+  [400, 600],
+  [0, 750],
+];
+
+const inimigos = [];
+
 function preload(){
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
   imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
   imagemGameOver = loadImage('imagens/assets/game-over.png');
+  ImagemTroll = loadImage('imagens/inimigos/troll.png');
+  ImagemVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
   
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
   somDoPulo = loadSound('sons/somPulo.mp3');
@@ -74,9 +129,16 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario,3);
-  personagem = new Personagem(matrizPersonagem,imagemPersonagem,0,110,135,220,270);
-  inimigo = new Inimigo(matrizInimigo,imagemInimigo,width - 52,52,52,104,104);
-  somDoJogo.loop  ();
+  personagem = new Personagem(matrizPersonagem,imagemPersonagem,0,30,110,135,220,270);
+  const inimigo = new Inimigo(matrizInimigo,imagemInimigo,width - 52,30,52,52,104,104,3,200);
+  const inimigoGrande = new Inimigo(matrizInimigoGrande,ImagemTroll,width *4,0,200,200,400,400,3,1500 );
+  const inimigoVoador = new Inimigo(matrizInimigoVoador,ImagemVoador,width -52,200,100,75,200,150,5,2000)
+
+    inimigos.push(inimigo);
+    inimigos.push(inimigoGrande);
+    inimigos.push(inimigoVoador);
+
+  //somDoJogo.loop();
   
   
 }
@@ -96,17 +158,23 @@ function draw() {
   cenario.move();
   personagem.exibe();
   personagem.aplicaGravidade();
-  inimigo.exibe();
-  inimigo.move();
   
-  if(personagem.estaColidindo(inimigo)){
-    console.log('colidiu');
-    
-    noLoop();
-    somDoJogo.pause();
-    somDerrota.play();
-    image(imagemGameOver,300,300,412,78);
-  }
+
+  inimigos.forEach(inimigo => {
+    inimigo.exibe();
+    inimigo.move();
+
+    if(personagem.estaColidindo(inimigo)){
+      console.log('colidiu');
+      
+     // noLoop();
+      somDoJogo.pause();
+      //somDerrota.play();
+     // image(imagemGameOver,windowWidth /3,windowHeight /2,412,78);
+    }
+  });
+  
+ 
   
 }
 
